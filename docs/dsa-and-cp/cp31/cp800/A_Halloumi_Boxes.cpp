8b046c154cfc27911ea49421790baed1278492ec
Kaushal Singh <80886710+17kaushalsingh@users.noreply.github.com>
2026-05-24 07:34:58 +0530

    Restart cp31

--- docs/dsa-and-cp/cp31/cp800/A_Halloumi_Boxes.cpp
+++ docs/dsa-and-cp/cp31/cp800/A_Halloumi_Boxes.cpp
@@ -1,41 +1,13 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <list>
-#include <deque>
-#include <array>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
-#include <functional>
-#include <string>
-#include <memory>
-#include <cstdlib>
-#include <cmath>
-#include <ctime>
-#include <chrono>
-#include <complex>
-#include <stdexcept>
-#include <exception>
-#include <type_traits>
-#include <bitset>
-#include <tuple>
-#include <iterator>
-#include <cassert>
-#include <queue>
+#include<bits/stdc++.h>
 using namespace std;
 
-bool f(vector<int>&arr, int k)
+bool isSorted(vector<int>&arr, int n)
 {
-    if (k >= 2) return true;
-
-    int n = arr.size();
     for (int i=1; i<n; i++)
     {
-        if (arr[i-1] > arr[i]) return false;
+        if (arr[i-1] > arr[i]) {
+            return false;
+        }
     }
 
     return true;
@@ -43,9 +15,6 @@ bool f(vector<int>&arr, int k)
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int t;
     cin >> t;
     while (t--)
@@ -58,8 +27,11 @@ int main()
         vector<int> arr(n);
         for (int i=0; i<n; i++) cin >> arr[i];
 
-        if (f(arr, k)) cout << "YES" << endl;
-        else cout << "NO" << endl; 
+        if (k >= 2 || isSorted(arr, n)) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
     }
 
     return 0;
--- docs/dsa-and-cp/cp31/cp800/A_Line_Trip.cpp
+++ docs/dsa-and-cp/cp31/cp800/A_Line_Trip.cpp
@@ -1,32 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <list>
-#include <deque>
-#include <array>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
-#include <functional>
-#include <string>
-#include <memory>
-#include <cstdlib>
-#include <cmath>
-#include <ctime>
-#include <chrono>
-#include <complex>
-#include <stdexcept>
-#include <exception>
-#include <type_traits>
-#include <bitset>
-#include <tuple>
-#include <iterator>
-#include <cassert>
-#include <queue>
-#
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -44,8 +16,7 @@ int main()
         vector<int> arr(n);
         for (int i=0; i<n; i++) cin >> arr[i];
 
-        int maxi = INT_MIN;
-        maxi = max(maxi, arr[0]);
+        int maxi = arr[0] - 0;
         for (int i=0; i<n-1; i++)
         {
             maxi = max(maxi, arr[i+1] - arr[i]);
